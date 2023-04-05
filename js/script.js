12928924342fe708b9503c62eebbc615bf199789
Patryk Świechowski <patryk.s9521@gmail.com>
2023-04-05 06:59:09 +0200

    Add computer move

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function printMessage(msg){
+/*function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
@@ -15,3 +15,48 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 let playerMove = 'papier';
 
 printMessage('Zagrałeś ' + computerMove + ' ! Wygrywasz!');
+
+let randomFraction = Math.random();
+
+printMessage('Wylosowany ułamek to: ' + randomFraction);
+
+let calculation = randomFraction * 9 + 1;
+
+printMessage('Ułamek pomnożony przez 9 i powiększony o 1: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);*/
+
+
+//Dokańczanie logiki gry papier, kamień, nożyce
+
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+	computerMove = 'kamień';
+}	if(randomNumber == 2){
+	computerMove = 'papier';
+}	else if(randomNumber == 3){
+	computerMove = 'nożyce';
+}
+
+
+
+
+printMessage('Mój ruch to: ' + computerMove);
